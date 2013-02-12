875fc32ad6cf0bbcdddb68f5c9eb7e688f2ec2d2
unknown <Nicolas@Nicolas-PC.(none)>
2013-02-12 14:15:04 +0100

    Configuration

--- DofusOrigin.Auth/Utilities/Config.cs
+++ DofusOrigin.Auth/Utilities/Config.cs
@@ -25,55 +25,7 @@ namespace DofusOrigin.Utilities
                 throw new Exception("Configuration file doesn't exist !");
 
             _config = new Configuration();
-            {
-                _config.LoadConfiguration("DofusOrigin.conf");
-                AddParameters();
-            }
-        }
-
-        private static void AddParameters()
-        {
-            //Status
-            _config.InsertElement("Status_inConsole", "true");
-            _config.InsertElement("Status_inFile", "false");
-
-            //Infos
-            _config.InsertElement("Infos_inFile", "false");
-            _config.InsertElement("Infos_inConsole", "true");
-
-            //Errors
-            _config.InsertElement("Errors_inFile", "true");
-            _config.InsertElement("Errors_inConsole", "true");
-
-            //Auth & Sync
-            _config.InsertElement("Auth_Ip", "127.0.0.1");
-            _config.InsertElement("Auth_Port", "485");
-            _config.InsertElement("Sync_Ip", "127.0.0.1");
-            _config.InsertElement("Sync_Port", "486");
-
-            //Client
-            _config.InsertElement("Login_Version", "1.29.1");
-
-            //Database
-            _config.InsertElement("Database_Server", "localhost");
-            _config.InsertElement("Database_User", "root");
-            _config.InsertElement("Database_Pass", "");
-            _config.InsertElement("Database_Name", "dofusorigin_auth");
-
-            //Cache
-            _config.InsertElement("Time_Accounts_Reload", "60000");
-            _config.InsertElement("Time_Servers_Reload", "60000");
-            _config.InsertElement("Time_Gifts_Reload", "60000");
-
-
-            //Queue
-            _config.InsertElement("Max_Clients_inQueue", "50");
-            _config.InsertElement("Client_Per_QueueRefresh", "10");
-            _config.InsertElement("Time_Queue_Reload", "5000");
-
-            //Subscription
-            _config.InsertElement("Max_Subscription_Time", "31536000000");
-            _config.InsertElement("Subscription_Time", "true");
+            _config.LoadConfiguration("DofusOrigin.conf");
         }
     }
 }
